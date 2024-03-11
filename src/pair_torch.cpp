1967bd6d57df814dd38f190df49bca5de45b7417
Niklas Kappel <niklas.kappel@kit.edu>
2024-03-11 16:46:22 +0100

    Bup, progress on compute function

--- src/pair_torch.cpp
+++ src/pair_torch.cpp
@@ -2,6 +2,7 @@
 
 #include "atom.h"
 #include "error.h"
+#include "neigh_list.h"
 
 #include <cstring>
 
@@ -17,7 +18,54 @@ PairTorch::PairTorch(LAMMPS *lmp) : Pair(lmp)
   manybody_flag = 1;
 }
 
-void PairTorch::compute(int eflag, int vflag) {}
+// TODO(niklas):
+// The `positions` and `types` tensors contains positions and types of all local atoms.
+// The `edge_index` tensor contains local indices of atoms described by the ML pair style only.
+// Passing these tensors into the model means the model must be invariant under the addition of unconnected atoms.
+// For ML/MM, it also means we crunch much larger tensors than necessary.
+// Alternatively, one could translate back and forth between local and ML indices and pass smaller `positions` and `types` tensors.
+
+void PairTorch::compute(int eflag, int vflag)
+{
+  ev_init(eflag, vflag);
+
+  // Types of all owned and ghost atoms in the local subdomain.
+  auto const *const type = atom->type;
+
+  // Positions of all owned and ghost atoms in the local subdomain.
+  auto const *const *const x = atom->x;
+
+  // Number of atoms for which neighbor lists have been created.
+  // This can not be greater than the number of owned atoms.
+  // It can be smaller, e.g. if a hybrid pair style is used.
+  auto const inum = list->inum;
+
+  // Local indices of all atoms for which neighbor lists have been created.
+  auto const *const ilist = list->ilist;
+
+  // Neighbor lists.
+  auto const *const *const firstneigh = list->firstneigh;
+
+  // Numbers of entries of all neighbor lists.
+  auto const *const numneigh = list->numneigh;
+
+  // Cached loop variables.
+  auto const *jlist = firstneigh[0];
+  auto jnum = numneigh[0];
+
+  for (int ii = 0, i = 0; ii < inum; ++ii) {
+    i = ilist[ii];
+    jlist = firstneigh[i];
+    jnum = numneigh[i];
+
+    for (int jj = 0, j = 0; jj < jnum; ++jj) {
+      j = jlist[jj];
+      j &= NEIGHMASK;    // NOLINT(hicpp-signed-bitwise): Required by LAMMPS.
+      // j is now the local index of a neighbor of the atom with local index i.
+      // Build the `edge_index` tensor.
+    }
+  }
+}
 
 void PairTorch::settings(int narg, char ** /*arg*/)
 {
